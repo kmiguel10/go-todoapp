3dadea70e2ce67439b53fc9c8187633419f13a03
Kent Miguel <kentmiguel10@gmail.com>
2020-09-08 14:06:10 -0400

    Create TodoItem Operation

--- todolist.go
+++ todolist.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"net/http"
 
+	"encoding/json"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -25,6 +27,16 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, `{"alive": true}`)
 }
 
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
+}
+
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetReportCaller(true)
@@ -39,5 +51,6 @@ func main() {
 	log.Info("Starting TodoList API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
+	router.HandleFunc("/todo", CreateItem).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
