a98ad38ace4aac403dc5498c6821fae78e7e8652
Kent Miguel <kentmiguel10@gmail.com>
2020-09-08 00:22:49 -0400

    Update todolist.go

--- todolist.go
+++ todolist.go
@@ -13,6 +13,12 @@ import (
 
 var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
+type TodoItemModel struct {
+	Id          int    `gorm:"primary_key"` //primary key
+	Description string //displays in front-end UI
+	Completed   bool   //determines if todo item is done or not
+}
+
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API health is ok")
 	w.Header().Set("Content-Type", "application/json")
@@ -26,6 +32,9 @@ func init() {
 
 func main() {
 	defer db.Close() //close db connection when main() func is returned
+
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
 
 	log.Info("Starting TodoList API server")
 	router := mux.NewRouter()
